ce6ce76497410cd2c5b6e792fbce4b7e172f1c17
Steven Toth <stoth@kernellabs.com>
2016-09-15 18:13:23 -0400

    Barebones SCTE104 parsing support.

--- src/core-packet-scte_104.c
+++ src/core-packet-scte_104.c
@@ -13,6 +13,19 @@ int dump_SCTE_104(struct vanc_context_s *ctx, void *p)
 	if (ctx->verbose)
 		printf("%s() %p\n", __func__, (void *)pkt);
 
+        struct splice_request_data *d = &pkt->sr_data;
+	struct single_operation_message *m = &pkt->so_msg;
+
+	printf("SCTE104: payloadDescriptor %02x\n", pkt->payloadDescriptorByte);
+	printf(" opID = %s\n", m->opID == INIT_REQUEST_DATA ? "init_request_data" : "UNSUPPORTED");
+	printf(" splice_insert_type = %s\n",
+		d->splice_insert_type == SPLICESTART_IMMEDIATE ? "spliceStart_immediate" :
+		d->splice_insert_type == SPLICEEND_IMMEDIATE ? "spliceEnd_immediate" : "UNSUPPORTED");
+
+	for (int i = 0; i < pkt->payloadLengthBytes; i++)
+		printf("%02x ", pkt->payload[i]);
+	printf("\n");
+
 	return KLAPI_OK;
 }
 
@@ -26,6 +39,60 @@ int parse_SCTE_104(struct vanc_context_s *ctx, struct packet_header_s *hdr, void
 		return -ENOMEM;
 
 	memcpy(&pkt->hdr, hdr, sizeof(*hdr));
+
+        pkt->payloadDescriptorByte = hdr->payload[0];
+        pkt->version               = (pkt->payloadDescriptorByte >> 3) & 0x03;
+        pkt->continued_pkt         = (pkt->payloadDescriptorByte >> 2) & 0x01;
+        pkt->following_pkt         = (pkt->payloadDescriptorByte >> 1) & 0x01;
+        pkt->duplicate_msg         = pkt->payloadDescriptorByte & 0x01;
+
+	/* We only support SCTE104 messages of type
+	 * single_operation_message() that are completely
+	 * self containined with a single VANC line, and
+	 * are not continuation messages.
+	 * Eg. payloadDescriptor value 0x08.
+	 */
+	if (pkt->payloadDescriptorByte != 0x08)
+		return -1;
+
+	/* First byte is the padloadDescriptor, the rest is the SCTE104 message...
+	 * up to 200 bytes in length item 5.3.3 page 7 */
+	for (int i = 0; i < 200; i++)
+		pkt->payload[i] = hdr->payload[1 + i];
+
+	struct single_operation_message *m = &pkt->so_msg;
+	m->opID             = pkt->payload[0] << 8 | pkt->payload[1];
+	m->messageSize      = pkt->payload[2] << 8 | pkt->payload[3];
+	m->result           = pkt->payload[4] << 8 | pkt->payload[5];
+	m->result_extension = pkt->payload[6] << 8 | pkt->payload[7];
+	m->protocol_version = pkt->payload[8];
+	m->AS_index         = pkt->payload[9];
+	m->message_number   = pkt->payload[10];
+	m->DPI_PID_index    = pkt->payload[11] << 8 | pkt->payload[12];
+
+	struct splice_request_data *d = &pkt->sr_data;
+	if (m->opID == INIT_REQUEST_DATA) {
+		d->splice_insert_type  = pkt->payload[13];
+		d->splice_event_id     = pkt->payload[14] << 24 |
+			pkt->payload[15] << 16 | pkt->payload[16] <<  8 | pkt->payload[17];
+		d->unique_program_id   = pkt->payload[18] << 8 | pkt->payload[19];
+		d->pre_roll_time       = pkt->payload[20] << 8 | pkt->payload[21];
+		d->brk_duration        = pkt->payload[22] << 8 | pkt->payload[23];
+		d->avail_num           = pkt->payload[24];
+		d->avails_expected     = pkt->payload[25];
+		d->auto_return_flag    = pkt->payload[26];
+	} else
+		return -1;
+
+	/* We only support spliceStart_immediate and spliceEnd_immediate */
+	switch (d->splice_insert_type) {
+	case SPLICESTART_IMMEDIATE:
+	case SPLICEEND_IMMEDIATE:
+		break;
+	default:
+		/* We don't support this splice command */
+		return -1;
+	}
 
 	if (ctx->callbacks && ctx->callbacks->scte_104)
 		ctx->callbacks->scte_104(ctx->callback_context, ctx, pkt);
--- src/libklvanc/vanc-scte_104.h
+++ src/libklvanc/vanc-scte_104.h
@@ -9,10 +9,53 @@
 extern "C" {
 #endif  
 
+#define INIT_REQUEST_DATA     0x01
+#define SPLICESTART_IMMEDIATE 0x02
+#define SPLICEEND_IMMEDIATE   0x04
+
+struct single_operation_message
+{
+	/* single_operation_message */
+	/* SCTE Spec table 7.1 */
+	unsigned short opID;
+	unsigned short messageSize;
+	unsigned short result;
+	unsigned short result_extension;
+	unsigned char protocol_version;
+	unsigned char AS_index;
+	unsigned char message_number;
+	unsigned short DPI_PID_index;
+};
+
+struct splice_request_data
+{
+	/* SCTE 104 Table 8-5 */
+	unsigned int splice_insert_type;
+	unsigned int splice_event_id;
+	unsigned short unique_program_id;
+	unsigned short pre_roll_time;	/* In 1/10's of a second */
+	unsigned short brk_duration;	/* In 1/10's of a second */
+	unsigned char avail_num;
+	unsigned char avails_expected;
+	unsigned char auto_return_flag;
+};
+
 struct packet_scte_104_s
 {
 	struct packet_header_s hdr;
-	int nr;
+
+	/* See SMPTE 2010:2008 page 5 */
+	unsigned char payloadDescriptorByte;
+	int version;
+	int continued_pkt;
+	int following_pkt;
+	int duplicate_msg;
+
+	unsigned char payload[256];
+	unsigned int payloadLengthBytes;
+
+	struct single_operation_message so_msg;
+	struct splice_request_data sr_data;
 };
 
 int dump_SCTE_104(struct vanc_context_s *ctx, void *p);
