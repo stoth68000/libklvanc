1009bcaa76d847db1f6141a69afe0c3b2b8778a1
Steven Toth <stoth@kernellabs.com>
2016-09-30 17:23:47 -0400

    Doxygen comments.

--- tools/klbitstream_readwriter.h
+++ tools/klbitstream_readwriter.h
@@ -1,3 +1,11 @@
+/**
+ * @file        klbitstream_readwriter.h
+ * @author      Kernel Labs Inc
+ * @brief       Simplistic bitstream reader/writer capable of supporting 1..32
+ *              1..32 bit writes or reads.
+ *              Buffers are used exclusively in either read or write mode, and cannot be combined.
+ */
+
 /* Copyright Kernel Labs Inc. 2016. All Rights Reserved. */
 
 #include <stdint.h>
@@ -9,24 +17,48 @@
 
 struct bs_context_s
 {
-	uint8_t  *buf;
-	uint32_t  buflen;	/* Bytes */
-	uint32_t  buflen_used;	/* Bytes */
+	/* Private, so not inspect directly. Use macros where necessary. */
+	uint8_t  *buf;		/* Pointer to the user allocated read/write buffer */
+	uint32_t  buflen;	/* Total buffer size - Bytes */
+	uint32_t  buflen_used;	/* Amount of data previously WRITTEN to the buffer. Invalid for read bitstreams. */
 	uint8_t   reg_used;	/* bits 1..8 */
 
-	/* A shift register */
+	/* An 8bit shift register */
 	/* Write bits are clocked in from LSB. */
 	/* Read bits are clocked out from the MSB. */
 	uint8_t  reg;
 };
+
+/**
+ * @brief       Helper Macro. Return the number of used bytes in the buffer (one valid for write buffers).
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @return      Return the number of used bytes in the buffer.
+ */
 #define bs_get_byte_count(ctx) ((ctx)->buflen_used)
+
+/**
+ * @brief       Helper Macro. Return the buffer address.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @return      Buffer address.
+ */
 #define bs_get_buffer(ctx) ((ctx)->buf)
 
+/**
+ * @brief       Allocate a new bitstream context, for read or write use.
+ * @return      struct bs_context_s *  The context itself, or NULL on error.
+ */
 static __inline__ struct bs_context_s * bs_alloc()
 {
 	return (struct bs_context_s *)calloc(1, sizeof(struct bs_context_s));
 }
 
+/**
+ * @brief       Save the entire buffer to file.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @param[in]   const char *fn  Output filename.
+ * @return      0 - Success
+ * @return      < 0 - Error
+ */
 static __inline__ int bs_save(struct bs_context_s *ctx, const char *fn)
 {
 	FILE *fh = fopen(fn, "wb");
@@ -39,16 +71,31 @@ static __inline__ int bs_save(struct bs_context_s *ctx, const char *fn)
 	return 0;
 }
 
+/**
+ * @brief       Destroy and deallocate a previously allocated context.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ */
 static __inline__ void bs_free(struct bs_context_s *ctx)
 {
 	free(ctx);
 }
 
+/**
+ * @brief       Initialize / reset a previously allocated context.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ */
 static __inline__ void bs_init(struct bs_context_s *ctx)
 {
 	memset(ctx, 0, sizeof(*ctx));
 }
 
+/**
+ * @brief       Associate a previously allocated user buffer 'buf' with the bitstream context.
+ *              The context will write data directly into this user buffer.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @param[in]   uint8_t *buf  Buffer the bistream write calls will modify
+ * @param[in]   uint32_t *buf  Buffer size in bytes.
+ */
 static __inline__ void bs_write_set_buffer(struct bs_context_s *ctx, uint8_t *buf, uint32_t lengthBytes)
 {
 	bs_init(ctx);
@@ -56,12 +103,23 @@ static __inline__ void bs_write_set_buffer(struct bs_context_s *ctx, uint8_t *bu
 	ctx->buflen = lengthBytes;
 }
 
+/**
+ * @brief       Associate a previously allocated user buffer 'buf' with the bitstream context.
+ *              The context will read data directly from this user buffer.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @param[in]   uint8_t *buf  Buffer the bistream will read from.
+ * @param[in]   uint32_t *buf  Buffer size in bytes.
+ */
 static __inline__ void bs_read_buffer_set(struct bs_context_s *ctx, uint8_t *buf, uint32_t lengthBytes)
 {
 	bs_write_set_buffer(ctx, buf, lengthBytes);
 }
 
-/* Clock into the LSB */
+/**
+ * @brief       Write a single bit into the previously associated user buffer.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @param[in]   uint32_t bit  A single bit.
+ */
 static __inline__ void bs_write_bit(struct bs_context_s *ctx, uint32_t bit)
 {
 	bit &= 1;
@@ -78,12 +136,25 @@ static __inline__ void bs_write_bit(struct bs_context_s *ctx, uint32_t bit)
 	}
 }
 
+/**
+ * @brief       Write multiple bits of data into the previously associated user buffer.
+ *              Writes are LSB justified, so the bits value 0x101, is nine bits.
+ *              Omitting this step could lead to a bistream thats one byte too short.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @param[in]   uint32_t bits  data pattern.
+ * @param[in]   uint32_t bitcount  number of bits to write
+ */
 static __inline__ void bs_write_bits(struct bs_context_s *ctx, uint32_t bits, uint32_t bitcount)
 {
 	for (int i = (bitcount - 1); i >= 0; i--)
 		bs_write_bit(ctx, bits >> i);
 }
 
+/**
+ * @brief       Flush any intermediate bits out to the buffer, once no omre data needs to be written.
+ *              This ensures that any dangling trailing bits are properly stuffer and written to the buffer.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ */
 static __inline__ void bs_write_buffer_complete(struct bs_context_s *ctx)
 {
 	if (ctx->reg_used > 0) {
@@ -94,7 +165,11 @@ static __inline__ void bs_write_buffer_complete(struct bs_context_s *ctx)
 //	printf("complete. buf size = %d bytes\n", ctx->buflen_used);
 }
 
-/* Clocked out from MSB */
+/**
+ * @brief       Read a single bit from the bitstream.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @return      uint32_t  a bit
+ */
 static __inline__ uint32_t bs_read_bit(struct bs_context_s *ctx)
 {
 	uint32_t bit;
@@ -117,6 +192,11 @@ printf("%d ", bit);
 	return bit;
 }
 
+/**
+ * @brief       Read between 1..32 bits from the bitstream.
+ * @param[in]   struct bs_context_s *ctx  bitstream context
+ * @return      uint32_t  bits
+ */
 static __inline__ uint32_t bs_read_bits(struct bs_context_s *ctx, uint32_t bitcount)
 {
 	uint32_t bits = 0;
