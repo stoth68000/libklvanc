8545f4b61b80510f07ad8cd7bc63c4bf849be874
Steven Toth <stoth@kernellabs.com>
2016-10-07 08:27:11 -0400

    Doxygen comments

--- src/libklvanc/vanc.h
+++ src/libklvanc/vanc.h
@@ -2,7 +2,13 @@
  * @file	vanc.h
  * @author	Steven Toth <stoth@kernellabs.com>
  * @copyright	Copyright (c) 2016 Kernel Labs Inc. All Rights Reserved.
- * @brief	TODO - Brief description goes here.
+ * @brief	Parse VANC lines, interpret data, call user callbacks with populated structures.\n
+ *              Callers allocate a usage context, assign callbacks to this context and then feed\n
+ *              VANC data into the library. VANC messages that are understood are parse, converted\n
+ *              into structs and if the user has registered a callback for that specific message type\n
+ *              then the structure is passed via the callback.\n\n
+ *              Callbacks should not attempt to modify or release the callback structs, releasing of\n
+ *              memory allocats is automatically taken care of by this library.
  */
 
 #ifndef _VANC_H
@@ -69,41 +75,49 @@ struct vanc_context_s
 	void *priv;
 };
 
-/* Create or destroy some basic application/library context */
 /**
- * @brief	TODO - Brief description goes here.
- * @param[in]	struct vanc_context_s **ctx - Brief description goes here.
+ * @brief	Create or destroy some basic application/library context.\n
+ *              The context is considered private and is likely to change.
+ * @param[out]	struct vanc_context_s **ctx - Context.
+ * @return      0 - Success
+ * @return      < 0 - Error
  */
 int vanc_context_create(struct vanc_context_s **ctx);
 
 /**
- * @brief	TODO - Brief description goes here.
- * @param[in]	struct vanc_context_s *ctx - Brief description goes here.
+ * @brief	Deallocate and destroy a context. See vanc_context_create()
+ * @param[in]	struct vanc_context_s *ctx - Context.
+ * @return      0 - Success
+ * @return      < 0 - Error
  */
 int vanc_context_destroy(struct vanc_context_s *ctx);
 
 /**
- * @brief	TODO - Brief description goes here.
- * @param[in]	struct vanc_context_s *ctx - Brief description goes here.
+ * @brief	Generate user visible context details to the console.
+ * @param[in]	struct vanc_context_s *ctx - Context.
+ * @return      0 - Success
+ * @return      < 0 - Error
  */
 int vanc_context_dump(struct vanc_context_s *ctx);
 
 /**
- * @brief	Parse a line of payload, trigger callbacks as necessary. lineNr is passed around and only
- *		used for reporting purposes, so we can figure out which line this came from in different
+ * @brief	Parse a line of payload, trigger callbacks as necessary. lineNr is passed around and only\n
+ *		used for reporting purposes, so we can figure out which line this came from in different\n
  *		callbacks and various parts of the reporting stack.
- * @param[in]	struct vanc_context_s *ctx - Brief description goes here.
- * @param[in]	unsigned int lineNr - Brief description goes here.
- * @param[in]	unsigned short *arr - Brief description goes here.
- * @param[in]	unsigned int len - Brief description goes here.
+ * @param[in]	struct vanc_context_s *ctx - Context.
+ * @param[in]	unsigned int lineNr - SDI line number the array data came from. Used for information / tracking purposes only.
+ * @param[in]	unsigned short *array - Array of SDI words (10bit) that the caller wants parsed.
+ * @param[in]	unsigned int len - Number of words in array.
+ * @return      0 - Success
+ * @return      < 0 - Error
  */
 int vanc_packet_parse(struct vanc_context_s *ctx, unsigned int lineNr, unsigned short *arr, unsigned int len);
 
 /**
  * @brief	TODO - Brief description goes here.
- * @param[in]	uint16_t *vanc - Brief description goes here.
- * @param[in]	int maxlen - Brief description goes here.
- * @param[in]	unsigned int linenr - Brief description goes here.
+ * @param[in]	uint16_t *array - Array of SDI words (10bit) that the caller wants parsed.
+ * @param[in]	int len - Number of words in array.
+ * @param[in]	unsigned int linenr - SDI line number the array data came from. Used for information / tracking purposes only.
  * @param[in]	int onlyvalid - Brief description goes here.
  */
 void vanc_dump_words_console(uint16_t *vanc, int maxlen, unsigned int linenr, int onlyvalid);
@@ -129,6 +143,8 @@ void vanc_dump_words_console(uint16_t *vanc, int maxlen, unsigned int linenr, in
  * @param[in]	uint16_t **dst - Brief description goes here.
  * @param[in]	uint16_t *dstWordCount - Brief description goes here.
  * @param[in]	uint32_t bitDepth - Brief description goes here.
+ * @return      0 - Success
+ * @return      < 0 - Error
  */
 int vanc_sdi_create_payload(uint8_t sdid, uint8_t did,
 	const uint8_t *src, uint16_t srcByteCount,
