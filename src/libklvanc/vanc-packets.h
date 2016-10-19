8538334ed074e05aa8aca226f63a526e8383f053
Steven Toth <stoth@kernellabs.com>
2016-10-19 09:59:20 -0400

    Explicitly state the library is LGPL V2.1 in all headers and source

--- src/libklvanc/vanc-packets.h
+++ src/libklvanc/vanc-packets.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016 Kernel Labs Inc. All Rights Reserved
+ *
+ * Address: Kernel Labs Inc., PO Box 745, St James, NY. 11780
+ * Contact: sales@kernellabs.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 /**
  * @file	vanc-packets.h
  * @author	Steven Toth <stoth@kernellabs.com>
