bb5ed3ef5dfda4f55ab8dbfe50708703cecab5c2
Devin Heitmueller <dheitmueller@kernellabs.com>
2016-12-19 12:19:08 -0500

    Add doxygen for new VANC functions.

--- src/libklvanc/vanc-lines.h
+++ src/libklvanc/vanc-lines.h
@@ -23,7 +23,14 @@
  * @file	vanc-lines.h
  * @author	Devin Heitmueller <dheitmueller@kernellabs.com>
  * @copyright	Copyright (c) 2016 Kernel Labs Inc. All Rights Reserved.
- * @brief	Routines for managing VANC lines containing one or more VANC packets
+ * @brief	Routines for managing VANC lines containing one or more VANC packets.
+ *
+ *              These routines allow a caller to take VANC packets and assemble a frame
+ *              composed of a group of VANC lines.  This includes:
+ *              1.  Ensuring no VANC packets with a given line/offset will overlap (moving VANC packets if necessary)
+ *              2.  Ensuring there are no gaps between VANC packets
+ *              3.  Ensuring the sum of all VANC packets does not overflow the line length
+ *              4.  Ensuring there are no illegal values in the VANC payload at a physical level
  */
 
 #ifndef _VANC_LINE_H
@@ -67,11 +74,64 @@ struct vanc_line_set_s
 	struct vanc_line_s *lines[MAX_VANC_LINES];
 };
 
+/**
+ * @brief	Create a VANC line
+ *
+ * @param[in]	int line_number - line number corresponding to where the line
+ *              placed within the field
+ * @return      struct vanc_line_s containing the line or NULL on error
+ */
 struct vanc_line_s *vanc_line_create(int line_number);
+
+/**
+ * @brief	Free a previously created vanc_line_s structure
+ * @param[in]	struct vanc_line_s *line - pointer to the line to be deleted.
+ *              This will also deallocate any VANC packets previously inserted into the line
+ */
 void vanc_line_free(struct vanc_line_s *line);
-void vanc_line_insert(struct vanc_line_set_s *vanc_lines, uint16_t *pixels,
+
+/**
+ * @brief	Insert a VANC packet into a VANC frame
+ *
+ * @param[in]	struct vanc_line_set_s *vanc_lines - pointer to set of VANC lines to insert into
+ * @param[in]	uint16_t *pixels - Pointer to array of pixels to insert.  For 10-bit pixels, these
+ *              should be 10-bit values inserted into 16-bit padded fields.
+ * @param[in]	int pixel_width - width of [pixels] argument, measured in number of samples
+ * @param[in]	int line_number - line number this VANC packet should appear on in the
+ *              resulting VANC frame
+ * @param[in]	int horizontal_offset - offset into active video area the packet should ideally
+ *              be inserted, measured in pixels
+ *
+ * @return      0 - Success
+ * @return      -ENOMEM - insufficient memory to store the VANC packet
+ */
+int vanc_line_insert(struct vanc_line_set_s *vanc_lines, uint16_t *pixels,
 		      int pixel_width, int line_number, int horizontal_offset);
-void generate_vanc_line(struct vanc_line_s *line, uint16_t **out_buf, int *out_len, int line_pixel_width);
+
+/**
+ * @brief	Generate pixel array representing a fully formed VANC line.  This
+ *              function will take in a vanc_line_s, format the VANC entries to ensure there
+ *              are no gaps or overlapping packets, and create a final pixel array which
+ *              can be colorspace converted and output over SDI.
+ *
+ * @param[in]	struct vanc_line_s *line - the VANC line to operate on
+ * @param[out]	uint16_t **out_buf - a pointer to the buffer the function will output into.  The
+ *              memory for the buffer will be allocated by the function, and the caller will need
+ *              to call free() to deallocate the resulting buffer.  For 10-bit video, the array
+ *              will contain 10-bit samples in 16-bit values.  Bit packing or Colorspace conversion
+ *              to other formats (such as V210) is the responsibility of the caller.
+ * @param[out]	int *out_len - the size of the resulting out_buf buffer.  This parameter allows
+ *              the caller to know how large out_buf is to avoid buffer overflows.  This value may
+ *              be shorter than the pixel width of the overall line.  Measured in number of 16-bit
+ *              samples (i.e. not bytes).
+ * @param[in]	int line_pixel_width - Size of the line the VANC will ultimately be inserted into,
+ *              measured in number of samples.  The function will not create an out_buf buffer which
+ *              is larger than this value, nor will it insert VANC packets which exceed the line
+ *              width specified.
+ * @return      0 - Success
+ * @return      -ENOMEM - insufficient memory to store the VANC packet
+ */
+int generate_vanc_line(struct vanc_line_s *line, uint16_t **out_buf, int *out_len, int line_pixel_width);
 
 #ifdef __cplusplus
 };
