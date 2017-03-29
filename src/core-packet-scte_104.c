d731f81bf5964f24e84e0157f99c3a3b311eecac
Devin Heitmueller <dheitmueller@kernellabs.com>
2017-03-29 14:13:56 -0400

    Fix endianness bug in segmentation event id field

--- src/core-packet-scte_104.c
+++ src/core-packet-scte_104.c
@@ -354,9 +354,7 @@ static int gen_avail_request_data(struct avail_descriptor_request_data *d,
 static unsigned char *parse_segmentation_request_data(unsigned char *p,
 						      struct segmentation_descriptor_request_data *d)
 {
-
-	d->event_id  = p[0] | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
-	p += 4;
+	d->event_id  = *(p + 0) << 24 | *(p + 1) << 16 | *(p + 2) <<  8 | *(p + 3); p += 4;
 	d->event_cancel_indicator = *(p++);
 	d->duration = p[0] | (p[1] << 8);
 	p += 2;
