46f56b8785c5659bc8bd10f904c66e2af1fb37be
Devin Heitmueller <dheitmueller@kernellabs.com>
2017-03-29 13:43:11 -0400

    Add missing file from previous commit

--- tools/klvanc_util.c
+++ tools/klvanc_util.c
@@ -29,6 +29,7 @@ extern int capture_main(int argc, char *argv[]);
 extern int parse_main(int argc, char *argv[]);
 extern int smpte2038_main(int argc, char *argv[]);
 extern int scte104_main(int argc, char *argv[]);
+extern int genscte104_main(int argc, char *argv[]);
 
 typedef int (*func_ptr)(int, char *argv[]);
 
@@ -43,6 +44,7 @@ int main(int argc, char *argv[])
 		{ "klvanc_parse",		parse_main, },
 		{ "klvanc_smpte2038",		smpte2038_main, },
 		{ "klvanc_scte104",		scte104_main, },
+		{ "klvanc_genscte104",		genscte104_main, },
 		{ 0, 0 },
 	};
 	char *appname = basename(argv[0]);
