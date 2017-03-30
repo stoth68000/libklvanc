8b0b18121798d9fb1c631290afcf9f753f2e5817
Devin Heitmueller <dheitmueller@kernellabs.com>
2017-03-30 16:51:07 -0400

    Pull later version of klbitstream_readwriter from libklscte35
    There were apparently fixes related to writebits when passing it
    a 64-bit int (as is common when serializing out PTS values).  Due
    to the way the includes are setup within libklscte35 the copy from
    libklvanc is being used instead.
    
    We should look to make klbitstream local to libklvanc so it isn't
    in the exported headers, but that can come as a later commit.  For
    now just sync the copies up so that we address the 64-bit issue.

--- src/libklvanc/klbitstream_readwriter.h
+++ src/libklvanc/klbitstream_readwriter.h
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2016 Kernel Labs Inc. All Rights Reserved
- *
- * Address: Kernel Labs Inc., PO Box 745, St James, NY. 11780
- * Contact: sales@kernellabs.com
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
 /**
  * @file        klbitstream_readwriter.h
  * @author      Steven Toth <stoth@kernellabs.com>
@@ -32,6 +11,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #ifndef KLBITSTREAM_READWRITER_H
 #define KLBITSTREAM_READWRITER_H
@@ -41,7 +21,7 @@ struct klbs_context_s
 	/* Private, so not inspect directly. Use macros where necessary. */
 	uint8_t  *buf;		/* Pointer to the user allocated read/write buffer */
 	uint32_t  buflen;	/* Total buffer size - Bytes */
-	uint32_t  buflen_used;	/* Amount of data previously WRITTEN to the buffer. Invalid for read bitstreams. */
+	uint32_t  buflen_used;	/* Amount of data previously read/written to the buffer. */
 	uint8_t   reg_used;	/* bits 1..8 */
 
 	/* An 8bit shift register */
@@ -51,7 +31,10 @@ struct klbs_context_s
 };
 
 /**
- * @brief       Helper Macro. Return the number of used bytes in the buffer (one valid for write buffers).
+ * @brief       Helper Macro. Return the number of used bytes in the buffer.
+ *              For a newly instantiated read buffer (or write buffer), this will always
+ *              be zero. If you want to under stand the size of the buffer, see
+ *              klbs_get_buffer_size();
  * @param[in]   struct klbs_context_s *ctx  bitstream context
  * @return      Return the number of used bytes in the buffer.
  */
@@ -63,6 +46,15 @@ struct klbs_context_s
  * @return      Buffer address.
  */
 #define klbs_get_buffer(ctx) ((ctx)->buf)
+
+/**
+ * @brief       Helper Macro. Return the total size of the buffer, regardless
+ *              of how much data has been read/written. Its the allocation size
+ *              we'll always return.
+ * @param[in]   struct klbs_context_s *ctx  bitstream context
+ * @return      Buffer address.
+ */
+#define klbs_get_buffer_size(ctx) ((ctx)->buflen)
 
 /**
  * @brief       Allocate a new bitstream context, for read or write use.
@@ -143,6 +135,8 @@ static __inline__ void klbs_read_set_buffer(struct klbs_context_s *ctx, uint8_t 
  */
 static __inline__ void klbs_write_bit(struct klbs_context_s *ctx, uint32_t bit)
 {
+	assert(ctx->buflen_used <= ctx->buflen);
+
 	bit &= 1;
 	if (ctx->reg_used < 8) {
 		ctx->reg <<= 1;
@@ -153,7 +147,6 @@ static __inline__ void klbs_write_bit(struct klbs_context_s *ctx, uint32_t bit)
 	if (ctx->reg_used == 8) {
 		*(ctx->buf + ctx->buflen_used++) = ctx->reg;
 		ctx->reg_used = 0;
-//		printf("Wrote %02x, size = %d\n", *(ctx->buf + ctx->buflen_used - 1), ctx->buflen_used);
 	}
 }
 
@@ -176,7 +169,7 @@ static __inline__ void klbs_write_byte_stuff(struct klbs_context_s *ctx, uint32_
  * @param[in]   uint32_t bits  data pattern.
  * @param[in]   uint32_t bitcount  number of bits to write
  */
-static __inline__ void klbs_write_bits(struct klbs_context_s *ctx, uint32_t bits, uint32_t bitcount)
+static __inline__ void klbs_write_bits(struct klbs_context_s *ctx, uint64_t bits, uint32_t bitcount)
 {
 	for (int i = (bitcount - 1); i >= 0; i--)
 		klbs_write_bit(ctx, bits >> i);
@@ -190,11 +183,9 @@ static __inline__ void klbs_write_bits(struct klbs_context_s *ctx, uint32_t bits
 static __inline__ void klbs_write_buffer_complete(struct klbs_context_s *ctx)
 {
 	if (ctx->reg_used > 0) {
-//printf("reg_used = %d\n", ctx->reg_used);
 		for (int i = ctx->reg_used; i <= 8; i++)
 			klbs_write_bit(ctx, 0);
 	}
-//	printf("complete. buf size = %d bytes\n", ctx->buflen_used);
 }
 
 /**
@@ -205,6 +196,7 @@ static __inline__ void klbs_write_buffer_complete(struct klbs_context_s *ctx)
 static __inline__ uint32_t klbs_read_bit(struct klbs_context_s *ctx)
 {
 	uint32_t bit = 0;
+	assert(ctx->buflen_used <= ctx->buflen);
 
 	if (ctx->reg_used == 0) {
 		ctx->reg = *(ctx->buf + ctx->buflen_used++);
@@ -216,27 +208,36 @@ static __inline__ uint32_t klbs_read_bit(struct klbs_context_s *ctx)
 		ctx->reg <<= 1;
 		ctx->reg_used--;
 	}
-#if 0
-printf("%d ", bit);
-	if (ctx->reg_used == 0)
-		printf(" -- ");
-#endif
 	return bit;
 }
 
 /**
- * @brief       Read between 1..32 bits from the bitstream.
+ * @brief       Read between 1..64 bits from the bitstream.
  * @param[in]   struct klbs_context_s *ctx  bitstream context
  * @return      uint32_t  bits
  */
-static __inline__ uint32_t klbs_read_bits(struct klbs_context_s *ctx, uint32_t bitcount)
+static __inline__ uint64_t klbs_read_bits(struct klbs_context_s *ctx, uint32_t bitcount)
 {
-	uint32_t bits = 0;
+	uint64_t bits = 0;
 	for (uint32_t i = 1; i <= bitcount; i++) {
 		bits <<= 1;
 		bits |= klbs_read_bit(ctx);
 	}
 	return bits;
+}
+
+/**
+ * @brief       Peek between 1..64 bits from the bitstream.
+ *              Each call to peek copies the context, advances it, without changing the
+ *              original context. As a result, consecutive peek calls will always return
+ *              the same content.
+ * @param[in]   struct klbs_context_s *ctx  bitstream context
+ * @return      uint32_t  bits
+ */
+static __inline__ uint64_t klbs_peek_bits(struct klbs_context_s *ctx, uint32_t bitcount)
+{
+	struct klbs_context_s copy = *ctx; /* Implicit struct copy */
+	return klbs_read_bits(&copy, bitcount);
 }
 
 /**
@@ -249,7 +250,5 @@ static __inline__ void klbs_read_byte_stuff(struct klbs_context_s *ctx)
 	while (ctx->reg_used > 0)
 		klbs_read_bit(ctx);
 }
-
-
 
 #endif /* KLBITSTREAM_READWRITER_H */
