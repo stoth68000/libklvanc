f69428bbc1563c90db7bd1122d7241bf00a9bb82
Devin Heitmueller <dheitmueller@ltnglobal.com>
2018-09-18 14:20:49 -0400

    Fix coverity 1473368
    The static analyzer was complaining that we weren't consistently
    checking the return value from the dump functions.  While we don't
    really care about it in the test tools, do it anyway to suppress
    the warning.

--- tools/parse.cpp
+++ tools/parse.cpp
@@ -208,7 +208,8 @@ static int cb_AFD(void *callback_context, struct klvanc_context_s *ctx,
 {
 	/* Have the library display some debug */
 	if (pkt_filtered(&pkt->hdr)) {
-		klvanc_dump_AFD(ctx, pkt);
+		if (klvanc_dump_AFD(ctx, pkt) < 0)
+			fprintf(stderr, "Failed to dump AFD packet");
 	}
 
 	return 0;
@@ -219,7 +220,8 @@ static int cb_EIA_708B(void *callback_context, struct klvanc_context_s *ctx,
 {
 	/* Have the library display some debug */
 	if (pkt_filtered(&pkt->hdr)) {
-		klvanc_dump_EIA_708B(ctx, pkt);
+		if (klvanc_dump_EIA_708B(ctx, pkt) < 0)
+			fprintf(stderr, "Failed to dump CEA-708 packet");
 	}
 
 	return 0;
@@ -230,7 +232,8 @@ static int cb_EIA_608(void *callback_context, struct klvanc_context_s *ctx,
 {
 	/* Have the library display some debug */
 	if (pkt_filtered(&pkt->hdr)) {
-		klvanc_dump_EIA_608(ctx, pkt);
+		if (klvanc_dump_EIA_608(ctx, pkt) < 0)
+			fprintf(stderr, "Failed to dump EIA-608 packet");
 	}
 
 	return 0;
@@ -241,7 +244,8 @@ static int cb_SCTE_104(void *callback_context, struct klvanc_context_s *ctx,
 {
 	/* Have the library display some debug */
 	if (pkt_filtered(&pkt->hdr)) {
-		klvanc_dump_SCTE_104(ctx, pkt);
+		if (klvanc_dump_SCTE_104(ctx, pkt) < 0)
+			fprintf(stderr, "Failed to dump SCTE-104 packet");
 	}
 	return 0;
 }
