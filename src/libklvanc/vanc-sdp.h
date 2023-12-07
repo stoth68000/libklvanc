a8d152b4ea6cfbd251077959061f751a0bf33da0
Steven Toth <steven.toth@ltnglobal.com>
2023-12-07 08:56:41 -0500

    sdp: early start at doxygen

--- src/libklvanc/vanc-sdp.h
+++ src/libklvanc/vanc-sdp.h
@@ -1,3 +1,11 @@
+
+/**
+ * @file	vanc-sdp.h
+ * @author	Yves De Muyter <yves@alfavisio.be>, tidy-ups: Steven Toth <stoth@kernellabs.com>
+ * @copyright	Copyright (c) 2023 Kernel Labs Inc. All Rights Reserved.
+ * @brief	WST / OP-47 / RDD8 - Subtitling Distribution Packet (SDP) Format
+ */
+
 #ifndef _VANC_SDP_H
 #define _VANC_SDP_H
 
@@ -12,6 +20,9 @@ enum klvanc_sdp_format_code_e
 	SDP_WSS_TELETEXT = 0x02
 };
 
+/**
+ * @brief	TODO - Brief description goes here.
+ */
 struct klvanc_sdp_desc_s
 {
 	uint8_t line;  // Line number, only 5 bits
@@ -19,6 +30,9 @@ struct klvanc_sdp_desc_s
 	uint8_t data[45]; // WSS Teletext data representing a subtitle line
 };
 
+/**
+ * @brief	TODO - Brief description goes here.
+ */
 struct klvanc_packet_sdp_s
 {
 	struct klvanc_packet_header_s hdr;
@@ -30,8 +44,18 @@ struct klvanc_packet_sdp_s
 
 int klvanc_dump_SDP(struct klvanc_context_s *ctx, void *p);
 
+/**
+ * @brief       Allocate a new SDP packet
+ * @param[out]	struct klvanc_packet_sdp_s **pkt - object created
+ * @return	0 - Success
+ * @return	< 0 - Error
+ */
 int klvanc_create_SDP(struct klvanc_packet_sdp_s **pkt);
 
+/**
+ * @brief       Free a previously allocated SDP packet.
+ * @param[in]	struct klvanc_packet_sdp_s *pkt - object to be freed
+ */
 void klvanc_destroy_SDP(struct klvanc_packet_sdp_s *pkt);
 
 int klvanc_convert_SDP_to_words(struct klvanc_packet_sdp_s *pkt, uint16_t **words, uint16_t *wordCount);
